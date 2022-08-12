366c786f2d07b2286819d382ff63b0193663d847
Tural Samadli <tsamadli4696@ada.edu.az>
2022-08-12 17:00:29 +0400

    admin panel and user messages are done

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
-use App\Http\Controllers\HomePageContoller;
+use App\Http\Controllers\HomePageController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\MessagesController;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,11 +21,15 @@ use App\Http\Controllers\AdminController;
 // Route::get('/control', function () {
 //     return view('new');
 // });
-Route::get('/',[HomePageContoller::class,'index'])->name('home');
-Route::get('/control',  [AdminController::class,'return'])->name('new');
-   Route::post('/control/about',[AdminController::class,'about'])->name('about');
-   Route::get('/control/message',[AdminController::class,'message'])->name('message');
-   Route::post('/control/contact',[AdminController::class,'info'])->name('contact');
-   Route::get('/control/contact',[AdminController::class,'contact'])->name('contact.index');
-   Route::get('/control/about',[AdminController::class,'index'])->name('about.index');
 
+Route::get('/',[HomePageController::class,'index'])->name('home');
+Route::get('/control',  [AdminController::class,'return'])->name('new');
+   // Route::get('/control/message',[AdminController::class,'message'])->name('message');
+   
+   // Route::get('/control/about', [AdminController::class,'contact'])->name('cont.index');
+   Route::get('/control/about', [AdminController::class,'index'])->name('user');
+   Route::get('/control/about', [AdminController::class,'store'])->name('store.index');
+   Route::get('/control/contact',[ContactController::class,'index'])->name('index');
+   Route::get('/control/contact',[ContactController::class,'update'])->name('contact.store');
+   Route::post('/', [HomePageController::class, 'user_messages'])->name('user_messages');
+   Route::get('/control/messages',[MessagesController::class,'index'])->name('message.index');

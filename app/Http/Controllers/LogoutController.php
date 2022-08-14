6464d1881bf9518c30bfda7b69010b340c04cf94
Tural Samadli <tsamadli4696@ada.edu.az>
2022-08-14 16:01:57 +0400

    logout is done

---
+++ app/Http/Controllers/LogoutController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\Session\Session;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LogoutController extends Controller
+{
+    public function logout(){
+        session()->flush();
+        Auth::logout();
+        return redirect()->route('login');
+    }
+}
